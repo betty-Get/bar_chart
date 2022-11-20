6ad01ef6f63d075a31c72310265ff70e23964543
@betty2926 <betelhem.getachew2926@gmail.com>
2022-11-19 22:17:01 -0800

    final push

--- src/utils/getDetailInfo.js
+++ src/utils/getDetailInfo.js
@@ -1,34 +1,33 @@
 export function getDetailInfo(timeline,index, socialMedia){
     let data = [];
-    let neg = [];
-    let pos = [];
-    let neu = [];
-    if(socialMedia == "twitter"){
-             timeline[index].externalTweets.map(e=>{
-               if(e.sentimentPolarityLabel = "POSITIVE"){
-                data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel})
+    
+    // if(socialMedia == "twitter"){
+    //          timeline[index].externalTweets.map(e=>{
+    //            if(e.sentimentPolarityLabel = "POSITIVE"){
+    //             data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel})
                 
-                  }
-                else if(e.sentimentPolarityLabel = "NEGATIVE"){
-                    // data = [];
-                    console.log(data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel}))
-                }
-                else{
-                    // data = [];
-                    data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel})
-                }
-            })
-        }
+    //               }
+    //             else if(e.sentimentPolarityLabel = "NEGATIVE"){
+    //                 // data = [];
+    //                 console.log(data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel}))
+    //             }
+    //             else{
+    //                 // data = [];
+    //                 data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel})
+    //             }
+    //         })
+      //  }
+      if(socialMedia == "twitter"){
+        timeline[index].externalTweets.map(e=>{
+            data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel})
+        })
+    }
     else{
             timeline[index].comments.map(e=>{
                 data.push({"comment": e.asReplyToPostMessage, "date":e.date.substring(0,10), "sentiment": e.sentimentPolarityLabel})
             })
         }
-    // if(socialMedia == "twitter"){
-    //     timeline[index].externalTweets.map(e=>{
-    //         data.push({"comment": e.tweet, "date":e.created, "sentiment": e.sentimentPolarityLabel})
-    //     })
-    // }
+    
 
    
 
